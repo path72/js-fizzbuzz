fd18db631575732d32a0868a25260f2b23d0e773
Salvatore Trimarchi <path72@gmail.com>
2021-02-25 13:28:56 +0100

    pulizie 1

--- js/main.js
+++ js/main.js
@@ -13,18 +13,16 @@ var N = 100,
 
 for (var i = 1; i<=N; i++) {
 
-  // console.log(i+' % 3 = '+(i % 3));
-  var j=i, msg, flag1=false, flag2=false;
+  var j=i, msg, flag1, flag2;
   flag1 = (i % n1 == 0) ? true : false;
   flag2 = (i % n2 == 0) ? true : false;
 
   if      ( flag1 && !flag2) msg = msg1;
   else if (!flag1 &&  flag2) msg = msg2;
   else if ( flag1 &&  flag2) msg = msg1 + msg2;
-  else msg = i;
+  else                       msg = i;
 
   console.log(j+': '+msg);
-
   document.getElementById('result_list').innerHTML += '<tr><td>'+j+'</td><td>'+msg+'</td><tr>';
 
 }
